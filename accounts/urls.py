b863d8d5fb61fdb3d46da74a33e20b09b897f03d
xjama <jamshidbekollanazarov@outlook.com>
2023-03-12 23:32:07 -0500

    xatolik

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import SignUpView
+from django.views.generic.base import TemplateView
+
+urlpatterns = [
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('profile/', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('logout/', TemplateView.as_view(template_name='home.html'), name='logout')
+]
--- computer_store/urls.py
+++ computer_store/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('products/', include('products.urls')),
     path('admin/', admin.site.urls),
 ]
